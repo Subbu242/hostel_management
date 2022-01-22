c932cb1ae2f15d0376846f866e614a213261d967
Neeraj R <67821150+Neeraj546@users.noreply.github.com>
2022-01-22 23:17:51 +0530

    Update add-courses.php

--- admin/add-courses.php
+++ admin/add-courses.php
@@ -39,7 +39,7 @@ echo"<script>alert('Course has been added successfully');</script>";
 <script type="text/javascript" src="js/jquery-1.11.3-jquery.min.js"></script>
 <script type="text/javascript" src="js/validation.min.js"></script>
 </head>
-<body style="background-image: url(../img/courses.png);">
+<body style="background-image: url(../img/courses.png);" class="bk-img">
 	<?php include('includes/header.php');?>
 	<div class="ts-main-content">
 		<?php include('includes/sidebar.php');?>
